b9188a5e5439348b9361d6d5a0bfc10fae9d2cfa
Diego Garcia <diego@puntdoc.es>
2012-03-08 22:18:24 +0100

    more house keeping and a new controller

---
+++ MyCMS.Web/Controllers/CreateUpdateArticleController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyCMS.Web.Controllers
+{
+    public class CreateUpdateArticleController : Controller
+    {
+        //
+        // GET: /CreateUpdateArticle/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
